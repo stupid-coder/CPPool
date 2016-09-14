bb0f38ad8106d7adb308e5917f0347bcf23eae7a
stupid-coder <stupid_coder@163.com>
2016-09-14 15:24:55 +0800

    Test program

--- test/main-logic/GenericKeyedObjectPool/TestGenericKeyedObjectPool.cc
+++ test/main-logic/GenericKeyedObjectPool/TestGenericKeyedObjectPool.cc
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <impl/GenericKeyedObjectPool.hpp>
+#include <sys/time.h>
+#include <cstdlib>
 
+#define LOOP_COUNT 10000
+
+std::string backends[] =
+{
+  "1","2","3","4","5"
+};
 
 template <typename K,typename V>
 class TestGenericKeyedObjectPoolFactory
@@ -26,11 +34,51 @@ public:
   }
 };
 
+void *test_routine(void *arg)
+{
 
+  CPPool::KeyedObjectPool<std::string,int> *pool = (CPPool::KeyedObjectPool<std::string,int>*)arg;
+
+  timeval seed;
+  gettimeofday(&seed,NULL);
+
+  srandom(seed.tv_usec);
+
+  for ( int i = 0; i < LOOP_COUNT; ++i )
+    {
+      std::string *backend = &backends[random()/sizeof(backends)];
+      int *client = pool->borrowObject(backend);
+      ++ *client;
+      pool->returnObject(backend,client);
+    }
+  pthread_exit(NULL);
+}
 
 int main(int argc, char *argv[])
 {
-
   CPPool::GenericKeyedObjectPool<std::string,int> pool(new TestGenericKeyedObjectPoolFactory<std::string,int>());
+
+  int test_num = 20;
+
+  if (argc>1) test_num = atoi(argv[1]);
+
+  pthread_t pid[test_num];
+
+  timeval begin,end;
+
+  gettimeofday(&begin,NULL);
+
+  for ( int i = 0; i < test_num; ++ i )
+    pthread_create(&pid[i],NULL,test_routine,&pool);
+
+  for ( int i = 0; i < test_num; ++ i )
+    pthread_join(pid[i],NULL);
+
+  gettimeofday(&end,NULL);
+
+  std::cout << "cost time:" << end.tv_sec-begin.tv_sec + (end.tv_usec-begin.tv_usec)/1000000 << std::endl;
+
+  pool.close();
+
   return 0;
 }
